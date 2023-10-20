2a0162d5b48f588b2bff27c2070220d521600158
Baptiste Chedeville <130366929+Baptched@users.noreply.github.com>
2023-10-20 21:11:15 +0200

    exploit: implémentation de la fonction pour decrypter selon la méthode acrostiche + tests unitaires

--- src/message1.py
+++ src/message1.py
@@ -33,3 +33,24 @@ def decrypte_cesar(message: str, nb_decalage: int) -> str:
             caractere_decrypte = caractere
         message_decrypte += caractere_decrypte
     return message_decrypte
+
+
+def decrypte_acrostiche(message: str) -> str:
+    """
+    Fonction permettant de déchiffrer un message chiffré par la méthode de 
+    l'acrostiche
+    Le message peut contenir des caractères spéciaux, des chiffres et des 
+    lettres minuscules ou majuscules
+
+    Args:
+        message (str): Le message chiffré à déchiffrer
+
+    Returns:
+        str: Le message décrypté
+    """
+    message_decrypte = ""
+    lignes = message.split("\n")
+    message_decrypte = ""
+    for ligne in lignes:
+        message_decrypte += ligne[0]
+    return message_decrypte
