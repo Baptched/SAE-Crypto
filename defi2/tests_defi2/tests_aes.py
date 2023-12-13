e16e7a2f651370414792fb4260a726f0c397b82f
Baptiste Chedeville <130366929+Baptched@users.noreply.github.com>
2023-12-13 15:10:02 +0100

    corvée: ajout tests unitaires module aes

--- defi2/tests_defi2/tests_aes.py
+++ defi2/tests_defi2/tests_aes.py
@@ -5,13 +5,14 @@ Module de tests pour le AES
 import sys
 import os
 import unittest
+from Crypto.Util.Padding import pad
+from Crypto.Cipher import AES
 
 ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
 sys.path.append(os.path.join(ROOT, 'src'))
 
-from aes import crypte_aes, decrypte_aes, crypte_aes_cbc, cassage_brutal
+from aes import crypte_aes, decrypte_aes, crypte_aes_cbc, cassage_brutal, decrypte_aes_cbc
 import constantes2 as c
-
 
 class TestAES(unittest.TestCase):
     """
@@ -45,6 +46,48 @@ class TestAES(unittest.TestCase):
                            0b00000110.to_bytes(c.NOMBRE_OCTETS_CLE, 'big'),
                            0b00000110.to_bytes(c.NOMBRE_OCTETS_CLE, 'big')),
             bytes)
+
+    def test_crypte_aes_cbc_invalid_key(self):
+        """
+        Fonction permettant de tester la fonction crypte_aes_cbc avec une clé invalide
+        """
+        iv = b'\x00' * 16
+        key = b'\x01' * 15
+        plaintext = b'Test message'
+        with self.assertRaises(Exception):
+            crypte_aes_cbc(iv, plaintext, key)
+
+    def test_decrypte_aes_cbc(self):
+        """
+        Fonction permettant de tester la fonction decrypte_aes_cbc
+        """
+        iv = b'\x00' * 16
+        key = b'\x01' * 16
+        plaintext = b'Test message'
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        ciphertext = cipher.encrypt(pad(plaintext, AES.block_size))
+        decrypted_text = decrypte_aes_cbc(iv, ciphertext, key)
+        self.assertEqual(decrypted_text, plaintext.decode('utf-8'))
+
+    def test_decrypte_aes_cbc_invalid_key(self):
+        """
+        Fonction permettant de tester la fonction decrypte_aes_cbc avec une clé invalide
+        """
+        iv = b'\x00' * 16
+        key = b'\x01' * 15 # Clé invalide
+        ciphertext = b'\x00' * 16
+        with self.assertRaises(Exception):
+            decrypte_aes_cbc(iv, ciphertext, key)
+
+    def test_decrypte_aes_cbc_invalid_ciphertext(self):
+        """
+        Fonction permettant de tester la fonction decrypte_aes_cbc avec un message invalide
+        """
+        iv = b'\x00' * 16
+        key = b'\x01' * 16
+        ciphertext = b'\x00' * 15 # Message invalide
+        with self.assertRaises(Exception):
+            decrypte_aes_cbc(iv, ciphertext, key)
 
     def test_cassage_brutal(self):
         """
